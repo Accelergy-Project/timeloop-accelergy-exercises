890ca812a2007fae1e51ce1f2efef19c3771fb1a
Nellie Wu <nelliewu@mit.edu>
2021-06-18 22:48:41 -0400

    update print script

--- workspace/exercises/2021.isca/scripts/02.2.1-spMspM-aggregated-stats.sh
+++ workspace/exercises/2021.isca/scripts/02.2.1-spMspM-aggregated-stats.sh
@@ -2,7 +2,7 @@ printf "\n=== Buffer ===\n"
 printf "\n --- A --- \n"
 grep "=== Buffer" *.stats.txt -A 80 | grep -e 'shape' -e ' storage c' -e 'format' -e reads -e fills
 printf "\n --- B --- \n"
-grep "=== Buffer" *.stats.txt -A 120 | grep "B:" -A 80 | grep -e 'shape' -e 'storage c' -e reads -e fills
+grep "=== Buffer" *.stats.txt -A 120 | grep "B:" -A 80 | grep -e 'shape' -e 'storage c' -e 'format' -e reads -e fills
 printf "\n --- Z --- \n"
 grep "=== Buffer" *.stats.txt -A 170 | grep "Z:" -A 80 | grep -e 'shape' -e 'data storage c' -e 'A[a-z]* scalar [reads|updates]' \
  -e 'Skipped scalar [reads|updates]'
