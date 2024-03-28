09545d3fea96e0d553ffb66a1dec865c538c8e1b
tanner-andrulis <andrulis@mit.edu>
2024-03-28 09:50:21 -0400

    Organize & Refactor

--- workspace/cheatsheets/generate_timeloop_spec.py
+++ workspace/cheatsheets/generate_timeloop_spec.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import os
 
     THIS_SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-    TOP_PATH = f"{THIS_SCRIPT_DIR}/../exercises/03_python_design_space_exploration_2024/top.yaml.jinja"
+    TOP_PATH = f"{THIS_SCRIPT_DIR}/../tutorial_exercises/03_python_design_space_exploration_2024/top.yaml.jinja"
     TARGET_FILE = f"{THIS_SCRIPT_DIR}/timeloop_spec.yaml"
     spec = tl.Specification.from_yaml_files(TOP_PATH)
     with open(TARGET_FILE, "w") as f:
--- workspace/generate_all_ref_outputs.sh
+++ workspace/generate_all_ref_outputs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cd baseline_designs
+cd example_designs
 python3 run_example_designs.py --clear-outputs
 python3 run_example_designs.py --architecture all --generate-ref-output
 
-cd ../exercises/01_accelergy_timeloop_2020_ispass/timeloop
+cd ../tutorial_exercises/01_accelergy_timeloop_2020_ispass/timeloop
 python3 run_example.py --clear-outputs
 python3 run_example.py all --generate-ref-output
 
